130b9babd190deedaa3a02f0d20960d23598bc7d
rishabh b <rishabh@rishabhs-MacBook-Air.local>
2025-05-09 20:16:03 -0400

    crop_edit

---
+++ CropEdit/CropEdit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CropEdit
+//
+//  Created by rishabh b on 5/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
